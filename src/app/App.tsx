04369e0d512f958ee76f998bc4a2236762463c31
DariaVor <daria_voronina@list.ru>
2025-07-25 17:25:37 +0300

    add dark theme

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,31 @@
+import { useEffect, useState } from 'react';
+import { ConfigProvider, theme as antdTheme } from 'antd';
 import NewsFeed from '../pages/NewsFeed/NewsFeed';
 
 const App = () => {
-  return <NewsFeed />;
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const darkQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    setIsDarkMode(darkQuery.matches);
+
+    const handleChange = (e: MediaQueryListEvent) => {
+      setIsDarkMode(e.matches);
+    };
+
+    darkQuery.addEventListener('change', handleChange);
+    return () => darkQuery.removeEventListener('change', handleChange);
+  }, []);
+
+  return (
+    <ConfigProvider
+      theme={{
+        algorithm: isDarkMode ? antdTheme.darkAlgorithm : antdTheme.defaultAlgorithm,
+      }}
+    >
+      <NewsFeed />
+    </ConfigProvider>
+  );
 };
+
 export default App;
